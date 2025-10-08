4f066e09a4172fcb3fb936f479b8bd7c7293e731
Sriram294 <sriram294@gmail.com>
2025-10-08 23:11:53 +0530

    feat: Export BACKEND_BASE constant for improved accessibility in the application

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,2 +1,2 @@
 export const TMDB_API_KEY = 'a6876d4405f24cb24dcbc98da14b0374';
-const BACKEND_BASE = import.meta.env.VITE_BACKEND_BASE;
+export const BACKEND_BASE = import.meta.env.VITE_BACKEND_BASE;
